194bf8e92b72485c64bad15f75ad8562daeb6f65
andmitrou <donmondi@gmail.com>
2018-03-10 21:48:49 +0200

    Add project files.

---
+++ AzureUtilities/IO/File.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureUtilities.IO
+{
+   public static class File
+    {
+        public static void Upload()
+        {
+           
+        }
+    }
+}
