6696da23cdad55fe950b92e4515838523f67651b
andmitrou <donmondi@gmail.com>
2018-03-11 14:33:46 +0200

    UploadAsBlob Implementation

--- AzureUtilities/IO/File.cs
+++ AzureUtilities/IO/File.cs
@@ -4,22 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
 
 namespace AzureUtilities.IO
 {
-   public static class File
+    public class AzureFile
     {
-        public static void UploadAsBlob(string filename, string blobContainer, string blobFileName)
+        private CloudStorageAccount _storageAccount;
+        private CloudBlobClient client;
+
+        public AzureFile(string storageConnectionString)
         {
-            if (System.IO.File.Exists(filename))
+            CloudStorageAccount.TryParse(storageConnectionString, out _storageAccount);
+
+            if (this._storageAccount == null)
+                throw new NullReferenceException("Storage Account has not been instantiated");
+
+            client = _storageAccount.CreateCloudBlobClient();
+        }
+
+        public void UploadAsBlob(string localfilename, string blobContainer)
+        {
+            var blobFileName = Path.GetFileName(localfilename);
+            UploadAsBlob(localfilename, blobContainer, blobFileName);
+        }
+
+        public void UploadAsBlob(string localfilename, string blobContainer, string newBlobFileName)
+        {
+            if (System.IO.File.Exists(localfilename))
                 throw new FileNotFoundException();
 
+            CloudBlobContainer container = client.GetContainerReference(blobContainer);
 
+            if (container == null)
+                throw new NullReferenceException("Container not found");
+
+            CloudBlockBlob blockblob = container.GetBlockBlobReference(newBlobFileName);
+
+            blockblob.UploadFromFile(localfilename, FileMode.Open);
         }
 
-        public static void UploadAsTable()
-        {//From Devel
-
-        }
     }
 }
